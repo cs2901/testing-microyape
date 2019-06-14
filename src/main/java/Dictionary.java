ec768fe543bfcdbe93004cd55c506f2e84dbecfd
Andrea Velasquez <andrea.velasquez@utec.edu.pe>
2019-06-14 13:07:53 -0500

    Implement dictionary

--- src/main/java/Dictionary.java
+++ src/main/java/Dictionary.java
@@ -1,2 +1,93 @@
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.*;
+
 public class Dictionary {
+    String text;
+    String encodedText;
+    String initialLanguage = "en";
+    List<String> urls = new ArrayList();
+    List<String> languages = Arrays.asList("es", "de", "fr");
+
+    public Dictionary(String text){
+        this.text = text;
+
+        try{
+            encodedText =  URLEncoder.encode(text, "UTF-8");
+        } catch (Exception e){
+            System.out.println("Could not encode text. Error: " + e);
+        }
+        createURLS();
+    }
+
+    private void createURLS() {
+
+        for (String targetLanguage : languages){
+            urls.add (
+                "https://translate.googleapis.com/translate_a/single?client=gtx" +
+                        "&sl=" + initialLanguage +
+                        "&tl=" + targetLanguage +
+                        "&dt=t&q=" + encodedText
+            );
+
+        }
+    }
+
+
+    public void translate(){
+        for (int i=0; i<urls.size(); ++i){
+            URL url;
+            try{
+                url = new URL(urls.get(i));
+                System.out.println(url);
+            }catch(Exception e){
+                System.out.println("Could not create URL. Error: " + e);
+                return;
+            }
+
+            HttpURLConnection con;
+            int status;
+            try {
+                con = (HttpURLConnection) url.openConnection();
+                con.setRequestMethod("GET");
+                con.setRequestProperty("Content-Type", "application/json");
+                status = con.getResponseCode();
+
+            } catch(Exception e){
+                System.out.println("Could not make request. Error: " + e);
+                return;
+            }
+
+            if (status!=200){
+                System.out.println("Get request returned status code " + status);
+                return;
+            }
+
+            String response;
+            try{
+                BufferedReader in = new BufferedReader(
+                        new InputStreamReader(con.getInputStream()));
+                String inputLine;
+                StringBuilder content = new StringBuilder();
+                while ((inputLine = in.readLine()) != null) {
+                    content.append(inputLine);
+                }
+                in.close();
+                response = content.toString();
+            }catch (Exception e){
+                System.out.println("Could not read request. Error: " + e);
+                return;
+            }
+
+            System.out.println("El response es: " + response);
+
+
+
+        }
+    }
+
 }
---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Dictionary dic = new Dictionary("hola como estas");
+
+        dic.translate();
+    }
+}
