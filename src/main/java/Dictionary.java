4ff1b03a8eec93f01e9ef87948eac9a018056940
Andrea Velasquez <andrea.velasquez@utec.edu.pe>
2019-06-16 23:56:18 -0500

    Finish dictionary

--- src/main/java/Dictionary.java
+++ src/main/java/Dictionary.java
@@ -1,3 +1,4 @@
+import org.json.JSONArray;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -5,89 +6,110 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Dictionary {
-    String text;
-    String encodedText;
-    String initialLanguage = "en";
-    List<String> urls = new ArrayList();
-    List<String> languages = Arrays.asList("es", "de", "fr");
+    private static final Logger logger = Logger.getLogger(Dictionary.class.getName());
+
+    private String encodedText;
+    private String initialLanguage = "en";
+    private List<URL> urls = new ArrayList<URL>();
+    private List<String> languagesCodes = Arrays.asList("es", "de", "fr");
+    private List<String> languages = Arrays.asList("spanish", "german", "french");
 
     public Dictionary(String text){
-        this.text = text;
-
         try{
             encodedText =  URLEncoder.encode(text, "UTF-8");
         } catch (Exception e){
-            System.out.println("Could not encode text. Error: " + e);
+            logger.log(Level.SEVERE, "Could not encode text. " + e.toString(), e);
         }
         createURLS();
     }
 
     private void createURLS() {
+        for (String targetLanguage : languagesCodes){
+            try{
+                urls.add (
+                        new URL (
+                        "https://translate.googleapis.com/translate_a/single?client=gtx" +
+                                "&sl=" + initialLanguage +
+                                "&tl=" + targetLanguage +
+                                "&dt=t&q=" + encodedText
+                        )
 
-        for (String targetLanguage : languages){
-            urls.add (
-                "https://translate.googleapis.com/translate_a/single?client=gtx" +
-                        "&sl=" + initialLanguage +
-                        "&tl=" + targetLanguage +
-                        "&dt=t&q=" + encodedText
-            );
-
+                );
+            }catch(Exception e){
+                logger.log(Level.SEVERE, "Could not create URL. " + e.toString(), e);
+                return;
+            }
         }
     }
 
+    private String makeRequest(URL url){
+        HttpURLConnection con;
+        int status;
+        try {
+            con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("GET");
+            con.setRequestProperty("Content-Type", "application/json");
+            con.setRequestProperty("User-Agent", "Chrome/74.0.3729.169");
+            status = con.getResponseCode();
 
-    public void translate(){
+        } catch(Exception e){
+            logger.log(Level.SEVERE, "Could not make request. " + e.toString(), e);
+            return "";
+        }
+
+        if (status!=200){
+            logger.log(Level.SEVERE, "Get request returned status code {0}", status);
+            return "";
+        }
+
+        String response;
+        try{
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            StringBuilder content = new StringBuilder();
+            while ((inputLine = in.readLine()) != null)  content.append(inputLine);
+            in.close();
+            response = content.toString();
+        }catch (Exception e){
+            logger.log(Level.SEVERE, "Could not read request. " + e.toString(), e);
+            return "";
+        }
+        return response;
+    }
+
+    private String parseResponse(String response){
+        JSONArray parsedResponse = new JSONArray(response);
+        for (int i=0; i<2; ++i) parsedResponse = (JSONArray) parsedResponse.get(0);
+
+        return (String) parsedResponse.get(0);
+    }
+
+
+    public HashMap<String, String> translate(){
+        HashMap<String, String> translations = new HashMap<String, String>();
+
         for (int i=0; i<urls.size(); ++i){
-            URL url;
-            try{
-                url = new URL(urls.get(i));
-                System.out.println(url);
-            }catch(Exception e){
-                System.out.println("Could not create URL. Error: " + e);
-                return;
-            }
-
-            HttpURLConnection con;
-            int status;
-            try {
-                con = (HttpURLConnection) url.openConnection();
-                con.setRequestMethod("GET");
-                con.setRequestProperty("Content-Type", "application/json");
-                status = con.getResponseCode();
-
-            } catch(Exception e){
-                System.out.println("Could not make request. Error: " + e);
-                return;
-            }
-
-            if (status!=200){
-                System.out.println("Get request returned status code " + status);
-                return;
+            if (makeRequest(urls.get(i)).length()==0) {
+                return translations;
             }
 
             String response;
-            try{
-                BufferedReader in = new BufferedReader(
-                        new InputStreamReader(con.getInputStream()));
-                String inputLine;
-                StringBuilder content = new StringBuilder();
-                while ((inputLine = in.readLine()) != null) {
-                    content.append(inputLine);
-                }
-                in.close();
-                response = content.toString();
-            }catch (Exception e){
-                System.out.println("Could not read request. Error: " + e);
-                return;
+            // Make request
+            try {
+                response = makeRequest(urls.get(i));
+            }catch(Exception e){
+                logger.log(Level.SEVERE, "Could not create URL. " + e.toString(), e);
+                return translations;
             }
 
-            System.out.println("El response es: " + response);
-
-
-
+            translations.put(languages.get(i), parseResponse(response));
         }
+        return translations;
     }
 
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,18 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Dictionary dic = new Dictionary("hola como estas");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter text (in english) to translate: ");
+        String text = scanner.nextLine();
+        scanner.close();
 
-        dic.translate();
+        System.out.println("Translating...");
+        Dictionary dic = new Dictionary(text);
+
+        for (HashMap.Entry<String, String> entry : dic.translate().entrySet()) {
+            System.out.println("Translation to " + entry.getKey() + ": " + entry.getValue());
+        }
     }
 }
